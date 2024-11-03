b8aa2e0aadf1591f06ceea051c8c8be7f3b28fb2
Arohee Bhoja <abhoja@fbi-van.wifi.local.cmu.edu>
2024-11-03 15:26:03 -0500

    made it even worse if thats even possible

--- StableMatching.lean
+++ StableMatching.lean
@@ -1,3 +1,46 @@
 -- This module serves as the root of the `StableMatching` library.
 -- Import modules here that should be built as part of the library.
 import StableMatching.Definitions
+
+-- DEFINITIONS --
+
+-- Given an instance of the SMP (A, B, pα, pβ)
+-- and a (partial) matching M : A × B
+
+-- (a, b) ∈ M is an *unstable* pair if ∃ (c, d) ∈ M such that:
+-- pα (a, d) > pα (a, b)
+-- pβ (a, d) > pα (c, d)
+-- (That is, a and d prefer each other to their current pairs)
+
+-- A (partial) matching M is *stable* if there are no unstable pairs.
+
+-- THEOREM --
+
+-- Given an instance of the SMP (A, B, pα, pβ)
+-- and a *partial* stable matching M : A × B of size k
+
+-- There exists a stable matching M' of size k+1.
+
+-- Probably two cases: someone in set A proposes to an unmatched member of set B (we're done)
+-- someone in set A proposes to a matched member of set B (we have a new matching of size k)
+-- someone unmatched will get proposed to eventually
+
+-- More stuff --
+
+-- Can apply this argument inductively to prove that there exists a stable
+-- matching for every instance of the problem (this is the Gale Shapley algo)
+
+-- Regardless of order of proposals we get the same result
+-- (this is not that interesting)
+
+-- Optimality and pessimality depending on which side is proposing
+-- Improvement lemma: if a member of proposed set is matched with someone
+-- in a partial stable matching, they'll only end up with someone better
+-- in a stable matching of increased size
+
+-- Stable roommates problem
+
+
+-- pa : α → β → β → Prop
+-- ∀ a ∈ A, IsLinearOrder (p a)
+-- pa a b1 b2 vs. (pa a b1) > (pa a b2)
--- StableMatching/Definitions.lean
+++ StableMatching/Definitions.lean
@@ -5,14 +5,19 @@ import Mathlib.Tactic
 
 section
 open Function
+open Finset
 
 variable
   (A : Finset α)
   (B : Finset β)
   (pa : α → β → ℕ)
   (pb : β → α → ℕ)
+  (pa' : α → ℕ → β)
+  (pb' : β → ℕ → α)
   (pa_linear : ∀ a ∈ A, Injective (pa a))
   (pb_linear : ∀ b ∈ B, Injective (pb b))
+  (pa_eq_pa' : ∀ a ∈ A, ∀ b ∈ B, pa' a (pa a b) = b)
+  (pb_eq_pb' : ∀ a ∈ A, ∀ b ∈ B, pb' b (pb b a) = a)
   (A_B_same_size : A.card = B.card)
 
 -- X is a subset of the cartesian product of A and B
@@ -34,6 +39,23 @@ variable
   (M : Finset (α × β))
   (M_partial : M.card < A.card)
   (M_stable : isStableMatching A B pa pb M)
+  (A' : Finset α)
+  (B' : Finset β)
+  (A'_def : A' ⊂ A ∧ (x ∈ A' → ∃ b ∈ B', (x, b) ∈ M))
+  (B'_def : B' ⊂ B ∧ (y ∈ B' → ∃ a ∈ A', (a, y) ∈ M))
+
+include A'_def
+theorem can_choose : ∃ a ∈ A, a ∉ A' := by
+  apply exists_of_ssubset
+  exact A'_def.left
+
+def choose_next' (a : α) (n : ℕ) :=
+  let b := pa' a n
+  if (∃ x ∈ A, (x, b) ∈ M) then -- if a is already matched to someone
+    (if pb b a > pb b x then b else choose_next' a (n + 1))
+    else b
+
+def choose_next (a : α) := choose_next' a 0
 
 -- Theorem 1: if there exists a partial stable matching M, we can find a stable matching M'
 -- with a higher variant score
@@ -41,103 +63,13 @@ theorem SM1 : ∃ (M' : Finset (α × β)), V pb M' > V pb M := by
   sorry
 
 
--- A stable matching with variant score (____) implies totality
-
--- THESE ARE BAD
--- Theorem 2: the upper bound of the variant is ???????
-theorem SM2 : ∀ (X : Finset (α × β)) (h : isStableMatching A B pa pb X),
-  V pb X ≤ (A.card) ^ 2 := by
+-- Theorem 2: a stable matching with a variant score (___) implies totality
+theorem SM2 (X : Finset (α × β)) : V pb X = 15 → X.card = A.card := by
   sorry
 
--- Theorem 3: this upper bound is only reached if the stable matching is total
-theorem SM3 (X : Finset (α × β)) : (isStableMatching A B pa pb X) ∧ (V pb X = A.card ^ 2) →
-  X.card = A.card := by
-  rintro ⟨h1, h2⟩
-  sorry
 
--- Theorem 4: given an instance of the SMP, there exists a total stable matching
-
-variable (A1 A2 : Finset α)
-
-open Finset
-
-example (h1 : A1 ⊆ A2) (h2 : A1.card < A2.card) :
-  ∃ x ∈ A2, x ∉ A1 := by
-  -- apply not_subset.mp
-  -- intro h3
-  -- have : A1 = A2 := by
-  --   exact Subset.antisymm h1 h3
-  -- subst this
-  -- simp at h2
-  apply exists_of_ssubset
-  rw [ssubset_iff_subset_ne]
-  use h1
-  contrapose! h2
-  rw [h2]
-
--- define sets of A and B in partial matching
--- A' ⊂ A, choose a thing in A that is not in A'
--- There is probably a theorem to do this
-
--- Pick a thing in A that is not in the matching -- can prove this exists
-
-
-theorem exists_larger : ∃ (M' : Finset (α × β)),
-        isStableMatching A B pa pb M' ∧ M'.card = M.card + 1 := by
-  -- Choose a ∈ A such that ¬∃ b ∈ B, ((a, b) ∈ M)
-  -- a proposes to b, the top person on its pref list who has not yet been proposed to
-  -- Case 1: b is unmatched. Then, M' = M ∪ (a, b). Done!
-  -- Case 2 (*): ∃ a' ∈ A, ((a', b) ∈ M). Then,
-  -- M'' = (M \ (a', b)) ∪ (a, b).
-  -- Now, a' proposes to b', the next person on its pref list after b
-  -- Case 1: b' is unmatched. Then, M' = M'' ∪ (a', b'), and we're done
-  -- Case 2: ∃ a'' ∈ A, ((a'', b') ∈ M). Then, we repeat the above step (*) until we find
-  -- an unmatched member of B. We're guaranteed to find such a member
-  -- since M is a partial matching and B is a finite set. Done! (how do we formalize this???)
-  sorry
+-- Theorem 3: can apply theorem 1 inductively to prove that for every instance
+-- of the SMP there exists a total stable matching ..
 
 
 end
-
--- DEFINITIONS --
-
--- Given an instance of the SMP (A, B, pα, pβ)
--- and a (partial) matching M : A × B
-
--- (a, b) ∈ M is an *unstable* pair if ∃ (c, d) ∈ M such that:
--- pα (a, d) > pα (a, b)
--- pβ (a, d) > pα (c, d)
--- (That is, a and d prefer each other to their current pairs)
-
--- A (partial) matching M is *stable* if there are no unstable pairs.
-
--- THEOREM --
-
--- Given an instance of the SMP (A, B, pα, pβ)
--- and a *partial* stable matching M : A × B of size k
-
--- There exists a stable matching M' of size k+1.
-
--- Probably two cases: someone in set A proposes to an unmatched member of set B (we're done)
--- someone in set A proposes to a matched member of set B (we have a new matching of size k)
--- someone unmatched will get proposed to eventually
-
--- More stuff --
-
--- Can apply this argument inductively to prove that there exists a stable
--- matching for every instance of the problem (this is the Gale Shapley algo)
-
--- Regardless of order of proposals we get the same result
--- (this is not that interesting)
-
--- Optimality and pessimality depending on which side is proposing
--- Improvement lemma: if a member of proposed set is matched with someone
--- in a partial stable matching, they'll only end up with someone better
--- in a stable matching of increased size
-
--- Stable roommates problem
-
-
--- pa : α → β → β → Prop
--- ∀ a ∈ A, IsLinearOrder (p a)
--- pa a b1 b2 vs. (pa a b1) > (pa a b2)
